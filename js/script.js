b8f5c0c52e87c0c21c2813319f0440084582afb8
Ana Garcia <anagdesignweb@gmail.com>
2024-03-28 18:05:43 +0100

    Add: js script - printCard function, printAllCards function

--- js/script.js
+++ js/script.js
@@ -17,28 +17,16 @@ let menu = [
   },
 ];
 
-
 //funcion menu desplegable
 
-botoncito.addEventListener("click",  () => {
-    nav.classList.toggle("visible");
-    nav.classList.toggle("invisible");
-  })
-
-
-menu.forEach((item) => {
-    divLinks.innerHTML +=`<a href="${item.url}">${item.name}</a>`;
+botoncito.addEventListener("click", () => {
+  nav.classList.toggle("visible");
+  nav.classList.toggle("invisible");
 });
 
-
-
-
-
-
-
-
-
-
+menu.forEach((item) => {
+  divLinks.innerHTML += `<a href="${item.url}">${item.name}</a>`;
+});
 
 /*
 ----Esta funcion es la basica, utilizando el bucle for y llamando a la funcion despues para que se muestre.
@@ -63,3 +51,90 @@ for (let i = 0; i < menu.length; i++) {
   console.log(menu[i].name);
 }
 */
+
+/*
+funcion para mostrar las cards en el carrusel de la seccion 3
+Funcion printCard -> pinta una sola card
+funcion printAllCards -> pinta con un bucle todas las cards
+*/
+
+const trackCarousel = document.getElementById("track");
+
+
+function printCard(img, title, description) {
+
+  trackCarousel.innerHTML += `<div class="card-1 carrusel">
+                                <div class="card-img">${img}
+                                </div>
+                                <div class="card-text">
+                                    <div>
+                                        <h5>${title}</h5>
+                                        <a href="">${description}</a>
+                                    </div>
+                                    <i class="material-symbols-outlined i-add">add</i>
+                                </div>
+                              </div>'`;
+}
+
+
+printCard("imagen", "titulillo", "blablabla");
+
+let arrayPrueba = [2,4,7,5,3];
+
+
+function printAllCards (){
+
+  for (let i = 0; i < arrayPrueba.length; i++) {
+    console.log(arrayPrueba[i]);
+    printCard(arrayPrueba[i], arrayPrueba[i], arrayPrueba[i])
+  }
+
+}
+printAllCards();
+
+/*
+Ejercicios:
+1. funcion de formateo de datos.
+function formatter / parser
+
+2. funcion de informacion al usuario segun el status.
+*/
+
+
+
+/* Funciones para cargar datos desde la API */
+
+let array = [];
+
+fetch("https://gkfibffviwvmphzqvuqe.supabase.co/rest/v1/stories?select=*", {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    apikey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrZmliZmZ2aXd2bXBoenF2dXFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA5ODQ5NTgsImV4cCI6MjAyNjU2MDk1OH0.M--1JO0f0zos59CcBc8oCPKZmz2su3qx0Z2hOqQK9c0",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrZmliZmZ2aXd2bXBoenF2dXFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA5ODQ5NTgsImV4cCI6MjAyNjU2MDk1OH0.M--1JO0f0zos59CcBc8oCPKZmz2su3qx0Z2hOqQK9c0",
+  },
+  mode: "cors",
+})
+  .then((res) => {
+    if (res.status === 200) {
+      res
+        .json()
+        .then((r) => {
+          debugger;
+          array = [...r];
+          console.log(array.length);
+          console.log(array[0]);
+        })
+        .catch((e) => {
+          debugger;
+        });
+    }
+  })
+  .catch((err) => {
+    debugger;
+  });
+
+
+
--- js/scriptCarrusel.js
+++ js/scriptCarrusel.js
@@ -1,36 +1,53 @@
-
 /*
 Necesito:
 1. mover la caja de las cards.
-
 Para ello tengo que tener :
 -variables para los botones.
 -funciones una para mover la caja hacia la derecha y otra hacia la izquierda.
 -variable que cargue las fotos *cuando consiga que funcionen los botones
-
 */
+
 
 let back = document.querySelector("#button-back").onclick=moveBack;
 let next = document.querySelector("#button-prev").onclick=moveNext;
 let track = document.querySelector("#track");
-
+let parent = document.querySelector(".section-3");
 let position = 0;
+let positionParent = parent.offsetLeft;
+let positionChild = track.offsetLeft;
+/*
+let positionParent = window.getComputedStyle(parent).left;
+let positionChild = window.getComputedStyle(track).left; 
+*/
 
-
-function positionInit(){
-    document.querySelector("#track").style.left=`0px`;
-}
+// function positionInit(){
+//     track.style.left = `${-300}px`;
+//     console.log(track.style.left);
+// };// EL PROBLEMA ESTA EN LA POSICION DE LA CAJA ROJA DESDE CSS PERO NO HAYYY MANERRRRRRRRRRA
 
 function moveNext(){
-    position = position -200;
-    debugger;
+    position -= 200;
     track.style.left = `${position}px`;
-    debugger;
+    //verPosicion();
+    console.log(position);
 };
 
 function moveBack() {
-    track.style.left = "200px";
+    position += 200;
+    track.style.left = `${position}px`;
+   // verPosicion();
 };
+
+function verPosicion(){
+    console.log(positionParent);
+    console.log(positionChild);
+}
 
 
 // positionInit();
+verPosicion();
+/* Ahora quiero ponerle topes a la caja,
+-si la caja esta en el borde de su izquierda que no siga saliendose de la caja, y
+-si la caja llega a su tope de la derecha que no siga aumentando 
+para ello tengo que comprobar la posicion actual de la caja y comparar*/
+
